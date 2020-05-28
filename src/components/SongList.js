fd70c4f9552c5b145c8a1d76a9434ebcf5ae3842
DariuszLegizynski <d.legizynski@gmail.com>
2020-05-28 22:46:49 +0200

    succesfully added the following hooks - useSelector and useDispatch

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,13 +1,19 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { selectSong } from "../actions";
 
 const SongList = () => {
+	const songs = useSelector((state) => state.songs);
+	const dispatch = useDispatch();
+
 	const renderSongList = () => {
 		return songs.map((song) => {
 			return (
 				<div className="item" key={song.title}>
 					<div className="right floated content">
-						<button className="ui button primary">Select</button>
+						<button className="ui button primary" onClick={() => dispatch(selectSong(song))}>
+							Select
+						</button>
 					</div>
 					<div className="content">{song.title}</div>
 				</div>
@@ -15,13 +21,7 @@ const SongList = () => {
 		});
 	};
 
-	const songs = useSelector((state) => state.songs);
-
 	return <div className="ui divided list">{renderSongList()}</div>;
 };
-
-// const mapStateToProps = (state) => {
-// 	return { songs: state.songs };
-// };
 
 export default SongList;
